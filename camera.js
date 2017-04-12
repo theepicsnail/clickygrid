6dc08490d27f45b0ac62bd8260a0398724fbe0f6
steven <theepicsnail@gmail.com>
2017-04-11 17:35:50 -0700

    Zooming!

--- camera.js
+++ camera.js
@@ -6,8 +6,38 @@ class Camera {
         this.ctx = this.canvas.getContext("2d");
         this.ctx.font = "30px Arial";
         this.ctx.fillStyle = "white";
+        this.zoom = 1;
 
         this.controls = new Hammer(this.canvas);
+        //this.controls.add(new Hammer.Pan());
+        //this.controls.add(new Hammer.Pinch());
+        //this.controls.add(new Hammer.Tap());
+        this.controls.get('pinch').set({ enable: true });
+
+        // Pinch and wheel zooming.
+        let baseZoom = this.zoom;
+        this.controls.on("pinchstart", (e) => {
+            baseZoom = this.zoom;
+        });
+        this.controls.on("pinch", (e) => {
+            this.zoom = baseZoom * e.scale;
+            this.recomputeCamera();
+        });
+        const T = this;
+
+        function wheelZoom(e) {
+
+            var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+
+            if (delta > 0)
+                T.zoom *= 1.03;
+            else
+                T.zoom /= 1.03;
+            T.recomputeCamera();
+        }
+
+        this.canvas.addEventListener("mousewheel", wheelZoom, false);
+        this.canvas.addEventListener("DOMMouseScroll", wheelZoom, false);
 
         let last = null;
 
@@ -20,14 +50,15 @@ class Camera {
                 last = null;
                 return;
             }
-            this.setCenter(this.x - e.center.x + last.x,
-                this.y - e.center.y + last.y);
+            this.setCenter(
+                this.x + (last.x-e.center.x)/this.zoom,
+                this.y + (last.y-e.center.y)/this.zoom);
 
             last = e.center;
         });
         this.controls.on("tap", (e) => {
-            const worldX = e.center.x + this.screenLeft;
-            const worldY = e.center.y + this.screenTop;
+            const worldX = e.center.x/ this.zoom + this.screenLeft;
+            const worldY = e.center.y/ this.zoom + this.screenTop;
             const chunkX = Math.floor(worldX / pixelsPerChunk);
             const chunkY = Math.floor(worldY / pixelsPerChunk);
             const tileX = Math.floor((worldX - chunkX * pixelsPerChunk) / tileSize);
@@ -57,12 +88,14 @@ class Camera {
     }
 
     recomputeCamera() {
+        this.zoom = Math.min(4, Math.max(.5, this.zoom));
+
         const w2 = this.canvas.width / 2;
         const h2 = this.canvas.height / 2;
-        this.screenLeft = Math.floor(this.x - w2);
-        this.screenTop = Math.floor(this.y - h2);
-        const screenRight = Math.ceil(this.x + w2);
-        const screenBot = Math.ceil(this.y + h2);
+        this.screenLeft = Math.floor(this.x - w2/ this.zoom);
+        this.screenTop = Math.floor(this.y - h2/ this.zoom);
+        const screenRight = Math.ceil(this.x + w2/ this.zoom);
+        const screenBot = Math.ceil(this.y + h2/ this.zoom);
 
         // Expand by 1 so that scrolling already has the next chunk.
         const chunkLeft = Math.floor(this.screenLeft / pixelsPerChunk) - 1;
@@ -70,7 +103,9 @@ class Camera {
         const chunkRight = Math.ceil(screenRight / pixelsPerChunk) + 1;
         const chunkBot = Math.ceil(screenBot / pixelsPerChunk) + 1;
 
-        this.ctx.setTransform(1, 0, 0, 1, -this.screenLeft, -this.screenTop);
+        this.ctx.setTransform(1,0,0,1,0,0);
+        this.ctx.scale(this.zoom, this.zoom);
+        this.ctx.translate(-this.screenLeft, -this.screenTop);
 
         let chunkLocations = new Set();
         for (let y = chunkTop; y < chunkBot; y++)
@@ -96,12 +131,12 @@ class Camera {
     }
 
     redrawChunk(chunk) {
-        const x = chunk.x * pixelsPerChunk;
-        const y = chunk.y * pixelsPerChunk;
+        const x = chunk.x*pixelsPerChunk;
+        const y = chunk.y*pixelsPerChunk;
         this.ctx.beginPath();
         this.ctx.drawImage(chunk.image, x, y);
         this.ctx.rect(x, y, pixelsPerChunk, pixelsPerChunk);
-        this.ctx.fillText(`(${chunk.x},${chunk.y})`, x + pixelsPerChunk / 2, y + pixelsPerChunk / 2);
+        this.ctx.fillText(`(${chunk.x},${chunk.y})`, x + pixelsPerChunk / 2, y + pixelsPerChunk/ 2);
         this.ctx.stroke();
     }
 }
